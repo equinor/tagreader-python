0788e622d2699f3b9c0f857df93ccc015e70ec97
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2019-05-15 08:12:40 +0200

    Reorganize ODBC handler tests

--- tests/test_odbchandlers.py
+++ tests/test_odbchandlers.py
@@ -1,7 +1,7 @@
 import pytest
-#import pandas as pd
-#from pyims import utils
-#from pyims.readertype import ReaderType
+import pandas as pd
+from pyims import utils
+from pyims.utils import ReaderType
 from pyims import IMSClient
 
 from pyims.odbc_handlers import list_aspen_servers,\
@@ -11,33 +11,6 @@ from pyims.odbc_handlers import list_aspen_servers,\
 
 from pyims.clients import get_server_address_aspen,\
     get_server_address_pi
-
-def test_list_all_aspen_servers():
-    res = list_aspen_servers()
-    assert isinstance(res, list)
-    assert len(res) >= 1
-    assert isinstance(res[0], str)
-    for r in res:
-        assert 3 <= len(r) <= 10
-
-def test_list_all_pi_servers():
-    res = list_pi_servers()
-    print(res)
-    assert isinstance(res, list)
-    assert len(res) >= 1
-    assert isinstance(res[0], str)
-    for r in res:
-        assert 3 <= len(r) <= 11
-
-def test_get_aspen_server_address():
-    host, port = get_server_address_aspen('SNA')
-    assert 'SNA-IMS.statoil.net' == host
-    assert 10014 == port
-
-def test_get_pi_server_address():
-    host, port = get_server_address_pi('ONO-IMS')
-    assert 'stj-w22.statoil.net' == host
-    assert 5450 == port
 
 def test_init():
     with pytest.raises(ValueError):
@@ -54,3 +27,71 @@ def test_init():
     assert isinstance(c.handler, PIHandlerODBC)
     c = IMSClient('snA', 'aspen')
     assert isinstance(c.handler, AspenHandlerODBC)
+
+@pytest.fixture(scope="module")
+def AspenHandler():
+    from pyims.odbc_handlers import AspenHandlerODBC
+    yield AspenHandlerODBC('thehostname', 1234)
+    # Insert any teardown functionality here
+
+def test_generate_connection_string(AspenHandler):
+    res = AspenHandler.generate_connection_string(AspenHandler.host, AspenHandler.port, 567890)
+    assert "DRIVER={AspenTech SQLPlus};HOST=thehostname;PORT=1234;READONLY=Y;MAXROWS=567890" == res
+
+def test_generate_tag_read_query(AspenHandler):
+    start_time = utils.datestr_to_datetime('2018-01-17 16:00:00')
+    stop_time = utils.datestr_to_datetime('2018-01-17 17:00:00')
+    ts = pd.Timedelta(1, unit='m')
+    res = AspenHandler.generate_read_query('thetag', None, start_time, stop_time, ts, ReaderType.INT)
+    expected = ("SELECT CAST(ts AS CHAR FORMAT 'YYYY-MM-DD HH:MI:SS') AS \"time\", value AS \"value\" FROM history "
+                "WHERE name = 'thetag' AND (ts BETWEEN '17-Jan-18 16:00:00' AND '17-Jan-18 17:00:00') AND "
+                "(request = 6) AND (period = 600) ORDER BY ts")
+    assert expected == res
+
+def test_list_all_aspen_servers():
+    res = list_aspen_servers()
+    assert isinstance(res, list)
+    assert len(res) >= 1
+    assert isinstance(res[0], str)
+    for r in res:
+        assert 3 <= len(r) <= 10
+
+def test_get_aspen_server_address():
+    host, port = get_server_address_aspen('SNA')
+    assert 'SNA-IMS.statoil.net' == host
+    assert 10014 == port
+
+@pytest.fixture(scope="module")
+def PIHandler():
+    from pyims.odbc_handlers import PIHandlerODBC
+    yield PIHandlerODBC('thehostname.statoil.net', 1234)
+
+def test_generate_connection_string(PIHandler):
+    res = PIHandler.generate_connection_string(PIHandler.host, PIHandler.port, 'das_server')
+    expected = ("DRIVER={PI ODBC Driver};Server=das_server;Trusted_Connection=Yes;Command Timeout=1800;"
+            "Provider Type=PIOLEDB;Provider String={Data source=thehostname;Integrated_Security=SSPI;};")
+    assert expected == res
+
+def test_generate_tag_read_query(PIHandler):
+    start_time = utils.datestr_to_datetime('2018-01-17 16:00:00')
+    stop_time = utils.datestr_to_datetime('2018-01-17 17:00:00')
+    ts = pd.Timedelta(1, unit='m')
+    res = PIHandler.generate_read_query('thetag', start_time, stop_time, ts, ReaderType.INT)
+    expected = ("SELECT CAST(value as FLOAT32) AS value, __utctime AS timestamp "
+                "FROM [piarchive]..[piinterp2] WHERE tag='thetag' AND "
+                "(time BETWEEN '17-Jan-18 16:00:00' AND '17-Jan-18 17:00:00') AND (timestep = '60s') ORDER BY timestamp")
+    assert expected == res
+
+def test_list_all_pi_servers():
+    res = list_pi_servers()
+    assert isinstance(res, list)
+    assert len(res) >= 1
+    assert isinstance(res[0], str)
+    for r in res:
+        assert 3 <= len(r) <= 11
+
+def test_get_pi_server_address():
+    host, port = get_server_address_pi('ONO-IMS')
+    assert 'stj-w22.statoil.net' == host
+    assert 5450 == port
+
