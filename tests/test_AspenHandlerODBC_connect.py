f34b144b1a0f0a963ac584319d6c8c3dce4133e5
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2020-05-29 14:31:27 +0200

    Rearrange test files

---
+++ tests/test_AspenHandlerODBC_connect.py
@@ -0,0 +1,22 @@
+
+import pytest
+import os
+
+from tagreader.odbc_handlers import list_aspen_servers
+
+is_GITHUBACTION = "GITHUB_ACTION" in os.environ
+
+if is_GITHUBACTION:
+    pytest.skip(
+        "All tests in module require connection to Aspen server",
+        allow_module_level=True
+    )
+
+
+def test_list_all_aspen_servers():
+    res = list_aspen_servers()
+    assert isinstance(res, list)
+    assert len(res) >= 1
+    assert isinstance(res[0], str)
+    for r in res:
+        assert 3 <= len(r) <= 20
