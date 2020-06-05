6f485e64e5ff84d3489aabc4d035963883b7d222
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2020-06-05 15:40:16 +0200

    Add search_tag() to PIHandlerWeb
    Fix certificate verification. Requires manual adding of intermediate
    certificate to chain.
    Write API queries using requests instead of API library.

--- tagreader/web_handlers.py
+++ tagreader/web_handlers.py
@@ -1,14 +1,6 @@
-import sys
-import warnings
 import requests
 from requests_kerberos import HTTPKerberosAuth, OPTIONAL
 from .utils import logging, urljoin
-
-
-# try:
-#     from osisoft.pidevclub.piwebapi.pi_web_api_client import PIWebApiClient
-# except ImportError:
-#     pass
 
 
 logging.basicConfig(
@@ -21,13 +13,12 @@ def get_auth():
 
 
 def list_pi_servers(host=r"https://pivision.equinor.com/piwebapi"):
-    # Suppress warning concerning use of verifySsl = False
     url = urljoin(host, "/assetservers")
     response = requests.get(url, auth=get_auth())
     if response.status_code == 200:
         server_list = []
-        for r in response.json()['Items']:
-            server_list.append(r['Name'])
+        for r in response.json()["Items"]:
+            server_list.append(r["Name"])
         return server_list
     elif response.status_code == 404:
         print("Not found")
@@ -38,7 +29,6 @@ class AspenHandlerWeb:
         self,
         host=r"https://aspenone.equinor.com/ProcessData",
         port=443,
-        max_rows=100000,
         options={},
     ):
         raise NotImplementedError
@@ -46,28 +36,60 @@ class AspenHandlerWeb:
 
 class PIHandlerWeb:
     def __init__(
-        self,
-        host=r"https://pivision.equinor.com/piwebapi",
-        port=443,
-        max_rows=100000,
-        options={},
+        self, host=None, port=None, options={},
     ):
-        self._max_rows = max_rows
+        self._max_rows = options.get("max_rows", 100000)
+        if host is None:
+            host = r"https://pivision.equinor.com/piwebapi"
         self.host = host
         self.port = port
-        # Suppress warning concerning use of verifySsl = False
-        import warnings
-        from urllib3.exceptions import InsecureRequestWarning
-        warnings.simplefilter("ignore", InsecureRequestWarning)
+        self.base_url = host
         self.session = requests.Session()
+        self.session.auth = get_auth()
 
     @staticmethod
     def generate_connection_string(host, *_, **__):
         raise NotImplementedError
 
     @staticmethod
+    def escape(s):
+        # https://techsupport.osisoft.com/Documentation/PI-Web-API/help/topics/search-queries.html  # noqa: E501
+        return s.translate(
+            str.maketrans(
+                {
+                    "+": r"\+",
+                    "-": r"\-",
+                    "&": r"\&",
+                    "|": r"\|",
+                    "(": r"\(",
+                    ")": r"\)",
+                    "{": r"\{",
+                    "}": r"\}",
+                    "[": r"\[",
+                    "]": r"\]",
+                    "^": r"\^",
+                    '"': r"\"",
+                    "~": r"\~",
+                    # Do not escape wildcard
+                    # "*": r"\*",
+                    ":": r"\:",
+                    "\\": r"\\",
+                    " ": r"\ "
+                }
+            )
+        )
+
+    @staticmethod
     def generate_search_query(tag=None, desc=None):
-        raise NotImplementedError
+        q = []
+        if tag is not None:
+            q.extend([f"name:{PIHandlerWeb.escape(tag)}"])
+        if desc is not None:
+            q.extend([f"description:{PIHandlerWeb.escape(desc)}"])
+        query = " AND ".join(q)
+
+        params = {"q": f"{query}", "scope": "pi:PINO"}
+        return params
 
     @staticmethod
     def generate_read_query(tag, start_time, stop_time, sample_time, read_type):
@@ -76,57 +98,34 @@ class PIHandlerWeb:
     def connect(self):
         self.session.auth = get_auth()
 
-    def search_tag(self, tag):
-        raise NotImplementedError
-
-    def read_tag(self, conn, tag, start_time, stop_time, sample_time, read_type):
-        raise NotImplementedError
-
-
-class PIHandlerWebOld:
-    def __init__(
-        self,
-        host=r"https://pivision.statoil.no/piwebapi",
-        port=443,
-        max_rows=100000,
-        options={},
-    ):
-        if "osisoft.pidevclub.piwebapi.pi_web_api_client" not in sys.modules:
-            raise ModuleNotFoundError(
-                "You need to install the PI Web API from OSISoft to use the PI web handler."  # noqa: E501
+    def search_tag(self, tag=None, desc=None):
+        params = self.generate_search_query(tag, desc)
+        url = urljoin(self.base_url, "search", "query")
+        print("******************************************")
+        print(url)
+        print(self.base_url)
+        done = False
+        ret = []
+        while not done:
+            # res = self.session.get(
+            #     "https://pivision.equinor.com/piwebapi/search/query", params=params
+            # )
+            res = self.session.get(
+                url, params=params
             )
-        warnings.warn("Unable to import PIWebApiClient")
-        self._max_rows = max_rows
-        self.host = host
-        self.port = port
-        # Suppress warning concerning use of verifySsl = False
-        # from urllib3.exceptions import InsecureRequestWarning
-        # warnings.simplefilter("ignore", InsecureRequestWarning)
-        # self.client = PIWebApiClient(host, useKerberos=True, verifySsl=False)
 
-    @staticmethod
-    def generate_connection_string(host, *_, **__):
-        raise NotImplementedError
-
-    @staticmethod
-    def generate_search_query(tag=None, desc=None):
-        raise NotImplementedError
-
-    @staticmethod
-    def generate_read_query(tag, start_time, stop_time, sample_time, read_type):
-        raise NotImplementedError
-
-    def connect(self):
-        """Not really needed, but nice to follow handler interface and have
-        a quick check whether we can actually connect to the home page.
-        """
-        try:
-            self.client.home.get()
-        except:
-            raise ConnectionError
-
-    def search_tag(self, tag):
-        raise NotImplementedError
+            if res.status_code != 200:
+                raise Exception
+            j = res.json()
+            for item in j["Items"]:
+                description = item["Description"] if "Description" in item else ""
+                ret.append((item["Name"], description))
+            next_start = int(j["Links"]["Next"].split("=")[-1])
+            if int(j["Links"]["Last"].split("=")[-1]) >= next_start:
+                params['start'] = next_start
+            else:
+                done = True
+        return ret
 
     def read_tag(self, conn, tag, start_time, stop_time, sample_time, read_type):
         raise NotImplementedError
--- tests/test_PIHandlerREST_connect.py
+++ tests/test_PIHandlerREST_connect.py
@@ -1,4 +1,85 @@
+import pytest
+import os
+
 from tagreader.web_handlers import list_pi_servers
+from tagreader.clients import IMSClient
+from tagreader.web_handlers import PIHandlerWeb
+
+
+is_GITHUBACTION = "GITHUB_ACTION" in os.environ
+
+if is_GITHUBACTION:
+    pytest.skip(
+        "All tests in module require connection to PI server", allow_module_level=True
+    )
+asset = "PINO"
+
+tags = {
+    "Float32": "BA:CONC.1",
+    "Digital": "BA:ACTIVE.1",
+    "Int32": "CDEP158",
+}
+
+interval = ["2020-04-01 11:05:00", "2020-04-01 12:05:00"]
+
+
+@pytest.fixture()
+def Client():
+    c = IMSClient(asset, "piweb")
+    c.cache = None
+    c.connect()
+    yield c
+    if os.path.exists(asset + ".h5"):
+        os.remove(asset + ".h5")
+
+
+def test_escape_chars():
+    assert (
+        PIHandlerWeb.escape('+-&|(){}[]^"~*:\\') == r"\+\-\&\|\(\)\{\}\[\]\^\"\~*\:\\"
+    )
+
+
+def test_generate_search_query():
+    assert PIHandlerWeb.generate_search_query("SINUSOID") == {
+        "q": "name:SINUSOID",
+        "scope": "pi:PINO",
+    }
+    assert PIHandlerWeb.generate_search_query(r"BA:*.1") == {
+        "q": r"name:BA\:*.1",
+        "scope": "pi:PINO",
+    }
+    assert PIHandlerWeb.generate_search_query(tag="BA:*.1") == {
+        "q": r"name:BA\:*.1",
+        "scope": "pi:PINO",
+    }
+    assert PIHandlerWeb.generate_search_query(desc="Concentration Reactor 1") == {
+        "q": r"description:Concentration\ Reactor\ 1",
+        "scope": "pi:PINO",
+    }
+    assert PIHandlerWeb.generate_search_query(
+        tag="BA:*.1", desc="Concentration Reactor 1"
+    ) == {
+        "q": r"name:BA\:*.1 AND description:Concentration\ Reactor\ 1",
+        "scope": "pi:PINO",
+    }
+
+
+def test_search_tag(Client):
+    res = Client.search_tag("SINUSOID")
+    assert 1 == len(res)
+    res = Client.search_tag("BA:*.1")
+    assert 5 == len(res)
+    [taglist, desclist] = zip(*res)
+    assert "BA:CONC.1" in taglist
+    assert desclist[taglist.index("BA:CONC.1")] == "Concentration Reactor 1"
+    res = Client.search_tag(tag="BA:*.1")
+    assert 5 == len(res)
+    res = Client.search_tag(desc="Concentration Reactor 1")
+    assert 1 == len(res)
+    res = Client.search_tag("BA*.1", "*Active*")
+    assert 1 == len(res)
+    res = Client.search_tag("BAS*")
+    assert 30 < len(res)
 
 
 def test_list_all_pi_servers():
