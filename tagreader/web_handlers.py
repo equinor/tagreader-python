5741834d56e5921e61201b93cebdbe9f28749315
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2020-06-12 14:58:24 +0200

    Create AspenHandlerWeb with basic structure

--- tagreader/web_handlers.py
+++ tagreader/web_handlers.py
@@ -27,12 +27,11 @@ def list_aspen_servers(
     auth=get_auth_aspen(),
     verify=False,
 ):
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
     url_ = urljoin(url, "DataSources")
     params = {"service": "ProcessData", "allQuotes": 1}
-
-    import urllib3
-
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     res = requests.get(url_, params=params, auth=auth, verify=False)
     if res.status_code == 200:
@@ -43,7 +42,7 @@ def list_aspen_servers(
 
 
 def list_pi_servers(url=r"https://piwebapi.equinor.com/piwebapi", auth=get_auth_pi()):
-    url_ = urljoin(url, "/dataservers")
+    url_ = urljoin(url, "dataservers")
     res = requests.get(url_, auth=auth)
     if res.status_code == 200:
         server_list = [r["Name"] for r in res.json()["Items"]]
@@ -54,10 +53,68 @@ def list_pi_servers(url=r"https://piwebapi.equinor.com/piwebapi", auth=get_auth_
 
 class AspenHandlerWeb:
     def __init__(
-        self,
-        server=None,
-        url=r"https://aspenone.equinor.com/ProcessData/AtProcessDataREST.dll",
-        options={},
+        self, url=None, server=None, options={},
+    ):
+        self._max_rows = options.get("max_rows", 100000)
+        if url is None:
+            url = r"https://aspenone-qa.equinor.com/ProcessData/AtProcessDataREST.dll"
+        self.base_url = url
+        self.dataserver = server
+        self.session = requests.Session()
+        self.session.verify = False
+        self.session.auth = get_auth_aspen()
+
+    @staticmethod
+    def generate_connection_string(host, *_, **__):
+        raise NotImplementedError
+
+    @staticmethod
+    def generate_search_query(tag=None, desc=None, server=None):
+        raise NotImplementedError
+
+    @staticmethod
+    def generate_read_query(
+        tag, start_time, stop_time, sample_time, read_type, metadata=None
+    ):
+        raise NotImplementedError
+
+    def verify_connection(self, server):
+        """Connects to the URL and verifies that the provided server exists.
+
+        :param server: Data server to look for
+        :type server: String
+        :raises ConnectionError: If connection fails
+        :return: True if server exists, False if not.
+        :rtype: Bool
+        """
+        url = urljoin(self.base_url, "Datasources")
+        params = {"service": "ProcessData", "allQuotes": 1}
+        res = self.session.get(url, params=params)
+        if res.status_code != 200:
+            raise ConnectionError
+        j = res.json()
+        for item in j["data"]:
+            if item["n"] == server:
+                return True
+        return False
+
+    def connect(self):
+        self.verify_connection(self.dataserver)
+
+    def search_tag(self, tag=None, desc=None):
+        raise NotImplementedError
+
+    def _get_tag_metadata(self, tag):
+        return {}  # FIXME
+
+    def _get_tag_unit(self, tag):
+        raise NotImplementedError
+
+    def _get_tag_description(self, tag):
+        raise NotImplementedError
+
+    def read_tag(
+        self, tag, start_time, stop_time, sample_time, read_type, metadata=None
     ):
         raise NotImplementedError
 
