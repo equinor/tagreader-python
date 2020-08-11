2f747a2f426a2af46015cd63260bad8fae723760
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2020-08-11 16:02:10 +0200

    Fix IP21 ODBC snapshot when using maps
    Slightly better handling of maps implemented.
    Now passes entire fetched mapdef dict to generate_read_query()
    instead of simply MAP_HistoryValue. This provides MAP_CurrentTimeStamp
    and MAP_CurrentValue for fetching snapshots for mapped tags.
    
    Cleaned up get_unit() and get_description() while refactoring helpers.
    The implementation of search() is still rather horrendous, but it needs
    to be refactored at some point anyway, so leaving it alone for now.
    All methods called _generate_*_for_search are used by search() only.

--- tagreader/odbc_handlers.py
+++ tagreader/odbc_handlers.py
@@ -71,8 +71,10 @@ class AspenHandlerODBC:
 
     @staticmethod
     def generate_read_query(
-        tag, mapping, start_time, stop_time, sample_time, read_type
+        tag, mapdef, start_time, stop_time, sample_time, read_type
     ):
+        if mapdef is None:
+            mapdef = {}
         if read_type in [
             ReaderType.COUNT,
             ReaderType.GOOD,
@@ -125,12 +127,11 @@ class AspenHandlerODBC:
         # For RAW: historyevent?
         # Ref https://help.sap.com/saphelp_pco151/helpdata/en/4c/72e34ee631469ee10000000a15822d/content.htm?no_cache=true  # noqa: E501
 
+        ts = "ts"
         if from_column == "aggregates":
             ts = "ts_start" 
         elif read_type == ReaderType.SNAPSHOT:
-            ts = "IP_INPUT_TIME"
-        else:
-            ts = "ts"
+            ts = mapdef.get("MAP_CurrentTimeStamp", "IP_INPUT_TIME")
 
         value = {
             ReaderType.MIN: "min",
@@ -143,7 +144,7 @@ class AspenHandlerODBC:
             ReaderType.NOTGOOD: "ng",
             ReaderType.TOTAL: "sum",
             ReaderType.SUM: "sum",
-            ReaderType.SNAPSHOT: "IP_INPUT_VALUE",
+            ReaderType.SNAPSHOT: mapdef.get("MAP_CurrentValue", "IP_INPUT_VALUE"),
         }.get(read_type, "value")
 
         query = [
@@ -155,8 +156,8 @@ class AspenHandlerODBC:
             start = start_time.strftime(timecast_format_query)
             stop = stop_time.strftime(timecast_format_query)
             query.extend([f"WHERE name = {tag!r}"])
-            if mapping is not None:
-                query.extend([f"AND FIELD_ID = FT({mapping!r})"])
+            if mapdef:
+                query.extend([f'AND FIELD_ID = FT({mapdef["MAP_HistoryValue"]!r})'])
             query.extend(
                 [
                     f"AND (ts BETWEEN {start!r} AND {stop!r})",
@@ -177,7 +178,7 @@ class AspenHandlerODBC:
         self.cursor = self.conn.cursor()
 
     @staticmethod
-    def _generate_query_get_mapdef_for_tag(tag):
+    def _generate_query_get_mapdef_for_search(tag):
         query = [
             "SELECT DISTINCT a.name as tagname, m.NAME, m.MAP_DefinitionRecord,",
             "m.MAP_IsDefault, m.MAP_Description, m.MAP_Units, m.MAP_Base, m.MAP_Range",
@@ -192,6 +193,20 @@ class AspenHandlerODBC:
         query.append("AND m.MAP_IsDefault = 'TRUE'")
         return " ".join(query)
 
+    def _get_mapdef_for_search(self, tag):
+        query = self._generate_query_get_mapdef_for_search(tag)
+        self.cursor.execute(query)
+        colnames = [col[0] for col in self.cursor.description]
+        rows = self.cursor.fetchall()
+        temp = [dict(zip(colnames, row)) for row in rows]
+        mapdef = {}
+        for t in temp:
+            if t['tagname'] not in mapdef:
+                mapdef[t['tagname']] = [t]
+            else:
+                mapdef[t['tagname']].append(t)
+        return mapdef
+
     @staticmethod
     def _generate_query_search_tag(mapdef, desc=None):
         if mapdef["MAP_DefinitionRecord"] is None:
@@ -205,31 +220,39 @@ class AspenHandlerODBC:
             query.append(f"AND {mapdef['MAP_Description']} like '{desc}'")
         return " ".join(query)
 
-    def _get_mapdef(self, tag):
-        query = self._generate_query_get_mapdef_for_tag(tag)
+    @staticmethod
+    def _generate_query_get_mapdefs(tag):
+        query = [
+            "SELECT m.NAME, m.MAP_DefinitionRecord, m.MAP_IsDefault,",
+            "m.MAP_Description, m.MAP_Units, m.MAP_Base, m.MAP_Range,",
+            "m.MAP_CurrentValue, m.MAP_CurrentTimeStamp, m.MAP_CurrentQuality,",
+            f'm.MAP_HistoryValue FROM "{tag}" t',
+            "LEFT JOIN atmapdef m ON t.definition = m.MAP_DefinitionRecord",
+        ]
+        return " ".join(query)
+
+    def _get_mapdefs(self, tag):
+        query = self._generate_query_get_mapdefs(tag)
         self.cursor.execute(query)
         colnames = [col[0] for col in self.cursor.description]
         rows = self.cursor.fetchall()
-        temp = [dict(zip(colnames, row)) for row in rows]
-        mapdef = {}
-        for t in temp:
-            if t["tagname"] not in mapdef:
-                mapdef[t["tagname"]] = [t]
-            else:
-                mapdef[t["tagname"]].append(t)
-        return mapdef
+        mapdefs = [dict(zip(colnames, row)) for row in rows]
+        return mapdefs
 
     def _get_specific_mapdef(self, tagname, mapping):
-        mapdef = self._get_mapdef(tagname)
-        for m in mapdef[tagname]:
-            if m["NAME"] == mapping:
-                return m
+        mapdefs = self._get_mapdefs(tagname)
+        for mapdef in mapdefs:
+            if mapdef["NAME"].lower() == mapping.lower():
+                return mapdef
         return None
 
     def _get_default_mapdef(self, tagname):
-        (tagname, _) = tagname.split(";") if ";" in tagname else (tagname, None)
-        mapdef = self._get_mapdef(tagname)
-        return mapdef[tagname][0]
+        #(tagname, _) = tagname.split(";") if ";" in tagname else (tagname, None)
+        mapdefs = self._get_mapdefs(tagname)
+        for mapdef in mapdefs:
+            if mapdef["MAP_IsDefault"] == "TRUE":
+                return mapdef
+        return None
 
     def search(self, tag=None, desc=None):
         if tag is None:
@@ -238,7 +261,7 @@ class AspenHandlerODBC:
         tag = tag.replace("*", "%") if isinstance(tag, str) else None
         desc = desc.replace("*", "%") if isinstance(desc, str) else None
 
-        mapdef = self._get_mapdef(tag)
+        mapdef = self._get_mapdef_for_search(tag)
 
         res = []
         for tagname in mapdef:  # TODO: Refactor
@@ -265,10 +288,10 @@ class AspenHandlerODBC:
     def _get_tag_unit(self, tag):
         (tagname, mapping) = tag.split(";") if ";" in tag else (tag, None)
         if mapping is None:
-            mapping = self._get_default_mapdef(tagname)
+            mapdef = self._get_default_mapdef(tagname)
         else:
-            mapping = self._get_specific_mapdef(tagname, mapping)
-        query = f"SELECT name, \"{mapping['MAP_Units']}\" as engunit FROM \"{tagname}\""
+            mapdef = self._get_specific_mapdef(tagname, mapping)
+        query = f"SELECT name, \"{mapdef['MAP_Units']}\" as engunit FROM \"{tagname}\""
         self.cursor.execute(query)
         res = self.cursor.fetchone()
         if not res.engunit:
@@ -287,18 +310,18 @@ class AspenHandlerODBC:
         ]
         query = " ".join(query)
         self.cursor.execute(query)
-        res = self.cursor.fetchall()
-        return res[0][1]
+        res = self.cursor.fetchone()
+        if not res.description:
+            res.description = ""
+        return res.description
 
     def read_tag(self, tag, start_time, stop_time, sample_time, read_type, metadata):
         (cleantag, mapping) = tag.split(";") if ";" in tag else (tag, None)
-        map_historyvalue = None
+        mapdef = dict()
         if mapping is not None:
-            c = self.conn.cursor()
-            c.execute(f'SELECT MAP_HistoryValue FROM "{mapping}"')
-            map_historyvalue = c.fetchone()[0]
+            mapdef = self._get_specific_mapdef(cleantag, mapping)
         query = self.generate_read_query(
-            cleantag, map_historyvalue, start_time, stop_time, sample_time, read_type
+            cleantag, mapdef, start_time, stop_time, sample_time, read_type
         )
         # logging.debug(f'Executing SQL query {query!r}')
         df = pd.read_sql(
