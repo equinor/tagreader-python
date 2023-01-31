c81d8115cfe1677160839f3c72787184b569ffff
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-01-31 13:46:16 +0100

    fix: catch exception when request does not return json (#133)

--- tagreader/web_handlers.py
+++ tagreader/web_handlers.py
@@ -57,8 +57,11 @@ def list_aspenone_sources(
 
     res = requests.get(url_, params=params, auth=auth, verify=verifySSL)
     if res.status_code == 200:
-        source_list = [r["n"] for r in res.json()["data"] if r["t"] == "IP21"]
-        return source_list
+        try:
+            source_list = [r["n"] for r in res.json()["data"] if r["t"] == "IP21"]
+            return source_list
+        except JSONDecodeError:
+            print("Did not return json")
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 401:
@@ -83,14 +86,19 @@ def list_piwebapi_sources(
 
     url_ = urljoin(url, "dataservers")
     res = requests.get(url_, auth=auth, verify=verifySSL)
+
     if res.status_code == 200:
-        source_list = [r["Name"] for r in res.json()["Items"]]
-        return source_list
+        try:
+            source_list = [r["Name"] for r in res.json()["Items"]]
+            return source_list
+        except JSONDecodeError:
+            print("Did not return json")
     elif res.status_code == 404:
         print("Not found")
     elif res.status_code == 401:
         print("Not authorized")
     res.raise_for_status()
+
 
 
 class AspenHandlerWeb:
