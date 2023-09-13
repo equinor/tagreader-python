48c401339f694e037279c81e5f9858276691e5be
Morten Dæhli Aslesen <masl@equinor.com>
2023-09-13 10:53:51 +0200

    feat!: remove ODBC support
    BREAKING CHANGE: ODBC based clients are no longer supported due to available REST services

--- tagreader/__init__.py
+++ tagreader/__init__.py
@@ -8,9 +8,6 @@ from tagreader.utils import (
     is_windows,
 )
 
-if is_windows():
-    from tagreader.odbc_handlers import list_aspen_servers, list_pi_servers
-
 if is_equinor():
     add_statoil_root_certificate()
 
