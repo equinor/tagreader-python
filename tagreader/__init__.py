18a3407b884eb018defe059f2c8b9386f61c5d7f
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2025-08-22 13:12:17 +0200

    refact: explicitly list imports in init.py

--- tagreader/__init__.py
+++ tagreader/__init__.py
@@ -4,11 +4,18 @@ from tagreader.utils import (
     ReaderType,
     add_equinor_root_certificate,
     is_equinor,
-    is_mac,
-    is_windows,
 )
 
 if is_equinor():
     add_equinor_root_certificate()
 
 from tagreader.__version__ import version as __version__
+
+__all__ = [
+    "IMSClient",
+    "list_sources",
+    "IMSType",
+    "ReaderType",
+    "add_equinor_root_certificate",
+    "__version__",
+]
